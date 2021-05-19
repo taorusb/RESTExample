68188ce1f30815f319f50e77739e64d4f67c444c
taorusb <61055630+taorusb@users.noreply.github.com>
2021-05-19 13:43:42 +0300

    Update V1.1__Create_users_table.sql

--- src/main/resources/db/migration/V1.1__Create_users_table.sql
+++ src/main/resources/db/migration/V1.1__Create_users_table.sql
@@ -1,7 +1,7 @@
 create table users
 (
     id       bigint       not null auto_increment,
-    username varchar(255) not null unique,
+    username varchar(255) not null,
     status   varchar(30),
 
     primary key (id)
